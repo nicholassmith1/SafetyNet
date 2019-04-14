1d2b4056933fa89c7c4e588953ed9fc3a61ee3c0
Nick Smith <nicholas_smith1@brown.edu>
2019-04-14 17:25:58 -0400

    Establish a first-pass at the SafetyNet architecture
    This commit adds the files and function calls that I expect we'll need
    for the project. All this is still completely up for debate, but
    even having these stubs should help us communicate about dependencies
    and progress.
    
    I've also added in some code for parsing the Okutama
    SingleActionTrackingLabels data annotations, which look like they support
    the pedestrian persistence that we're looking for. This should help
    me start development before we have the pedestrian segmentation working.

---
+++ code/helpers.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+def collate_pedestrians(pedestrians_prev, bounding_boxes):
+    pass
+
+def classify_pedestrian(pedestrian_vel_abs):
+    pass
+
+def estimate_epicenter(epicenter_prev, pedestrian_class, pedestrian_pos, pedestrian_vel_abs):
+    pass
---
+++ code/segmentation.py
@@ -0,0 +1,10 @@
+
+import numpy as np
+def find_pedestrians(frame_cur):
+    """
+    @returns    a Nx4 numpy array, in which each row represents a pedestrian
+                discovered in frame_cur, with the columns representing the
+                (x, y, width, height) of the bounding box of the pedestrian.
+    """
+    # TODO - replace stub
+    return np.empty((0, 4))
