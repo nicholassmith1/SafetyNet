bc5902a4166545c63ecba219cb51bfc001816e4f
Nick Smith <nicholas_smith1@brown.edu>
2019-04-16 20:00:13 -0400

    Implement some point correspondence and pose estimation
    Point correspondence looks pretty good. Pose deltas look questionable
    (a little high, maybe, depending on frame rate). May need to implement
    some visualization and look at a couple frames individually.

--- code/helpers.py
+++ code/helpers.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 
 def collate_pedestrians(pedestrians_prev, bounding_boxes):
     pass
@@ -8,3 +9,21 @@ def classify_pedestrian(pedestrian_vel_abs):
 
 def estimate_epicenter(epicenter_prev, pedestrian_class, pedestrian_pos, pedestrian_vel_abs):
     pass
+
+
+# From https://www.learnopencv.com/rotation-matrix-to-euler-angles/
+def rotationMatrixToEulerAngles(R) :
+    # assert(isRotationMatrix(R))
+    sy = math.sqrt(R[0,0] * R[0,0] +  R[1,0] * R[1,0])
+    singular = sy < 1e-6
+ 
+    if  not singular:
+        x = math.atan2(R[2,1] , R[2,2])
+        y = math.atan2(-R[2,0], sy)
+        z = math.atan2(R[1,0], R[0,0])
+    else :
+        x = math.atan2(-R[1,2], R[1,1])
+        y = math.atan2(-R[2,0], sy)
+        z = 0
+ 
+    return np.array([x, y, z])
--- code/photogrammetry.py
+++ code/photogrammetry.py
@@ -1,7 +1,59 @@
 import cv2
 import numpy as np
+import matplotlib.pyplot as plt
+from helpers import (rotationMatrixToEulerAngles)
 
 def calc_drone_vel(intr, frame_prev, frame_cur, bounding_boxes):
+    # Get key features
+    #sift = cv2.xfeatures2d.SIFT_create()
+    #surf = cv2.xfeatures2d.SURF_create()
+    orb = cv2.ORB_create(nfeatures=1500)
+
+    kp_cur, desc_cur = orb.detectAndCompute(frame_cur, None)
+    kp_prev, desc_prev = orb.detectAndCompute(frame_prev, None)
+
+    # create BFMatcher object
+    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+
+    # Match descriptors. Hopefully improve RANSAC performance
+    matches = bf.match(desc_prev,desc_cur)
+
+    # # Sort them in the order of their distance.
+    # matches = sorted(matches, key = lambda x:x.distance)
+
+    # Debug
+    img = cv2.drawMatches(frame_prev, kp_prev, frame_cur, kp_cur, matches, None)
+    plt.imshow(img),plt.show()
+
+    kpc = np.empty((len(matches), 2))
+    kpp = np.empty((len(matches), 2))
+    for i in range(len(matches)):
+        # print('-----')
+        # print(matches[i].imgIdx)
+        # print(matches[i].queryIdx)
+        # print(matches[i].trainIdx)
+
+        # FIXME - backwards?
+        kpc[i, 0] = kp_cur[matches[i].queryIdx].pt[0]
+        kpc[i, 1] = kp_cur[matches[i].queryIdx].pt[1]
+        kpp[i, 0] = kp_prev[matches[i].trainIdx].pt[0]
+        kpp[i, 1] = kp_prev[matches[i].trainIdx].pt[1]
+
+    # Calculate the essential matrix
+    focal = (intr[0, 0] + intr[1, 1]) / 2.0
+    oc = (intr[0, 2], intr[1, 2])
+    E, mask = cv2.findEssentialMat(kpc, kpp, focal=focal, pp=oc, method=cv2.RANSAC, prob=0.999, threshold=1.0)
+    
+    retval, R, t, mask = cv2.recoverPose(E, kpc, kpp, intr, mask);
+
+    # TODO - convert to velocities using frame rate
+    # TODO - validate output
+
+    # print(R)
+    # print(rotationMatrixToEulerAngles(R))
+    # print(t)
+
+
     pass
 
 
