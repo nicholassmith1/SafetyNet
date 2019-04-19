12ff22ff1fad587ecfed808704df057a59e39af8
Nick Smith <nicholas_smith1@brown.edu>
2019-04-19 16:41:18 -0400

    WIP

--- code/main.py
+++ code/main.py
@@ -7,8 +7,10 @@ import logging
 import signal
 import sys
 import cv2
+import matplotlib.pyplot as plt
+import pickle
 
-from photogrammetry import (calc_drone_vel,
+from photogrammetry import (calc_drone_pos_delta,
         calc_disparity_map,
         calc_pedestrian_position,
         calc_pedestrian_velocity,
@@ -20,18 +22,64 @@ from helpers import (collate_pedestrians,
         estimate_epicenter)
  
 log = logging.getLogger(__name__)
- 
+
+# debug stuff
+time = [0.0]
+pos_x = [0.0]
+pos_y = [0.0]
+pos_z = [0.0]
+pos_R = [[[0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0]]]
+
+def moving_average(data, N):
+    return np.convolve(data, np.ones((N,))/N, mode='same')
 
 def process_frame(intr, dt, frame_cur, frame_prev, bounding_boxes=None):
     pedestrians_prev = None  # FIXME
     epicenter_prev = None
+
+    time.append(time[len(time) - 1] + dt)
+
+    # # DEBUG skip ahead
+    # if time[len(time) - 1] < 17:
+    #     return
 
     # Get an array of pedestrian bounding boxes.
     if bounding_boxes is None:
         bounding_boxes = find_pedestrians(frame_cur)
 
     # Calculate the drone instanenous velocity, drone FOR
-    drone_vel = calc_drone_vel(intr, frame_prev, frame_cur, bounding_boxes)
+    confidence, R, t = calc_drone_pos_delta(intr, frame_prev, frame_cur, bounding_boxes)
+    # debug stuff
+    
+    if (confidence > 0.5):
+        pos_x.append(t[0])
+        pos_y.append(t[1])
+        pos_z.append(t[2])
+        pos_R.append(R)
+    else:
+        pos_x.append(pos_x[len(pos_x) - 1])
+        pos_y.append(pos_y[len(pos_y) - 1])
+        pos_z.append(pos_z[len(pos_z) - 1])
+        pos_R.append(pos_R[len(pos_R) - 1])
+
+    if time[len(time) - 1] > 70:
+        import matplotlib.pyplot as plt
+        import pandas as pd
+
+        ma_pos_x = moving_average(np.array(pos_x), 15)
+        ma_pos_y = moving_average(np.array(pos_y), 15)
+        ma_pos_z = moving_average(np.array(pos_z), 15)
+
+        df=pd.DataFrame({'t': time, 'x': ma_pos_x, 'y': ma_pos_y, 'z': ma_pos_z })
+        plt.plot('t', 'x', data=df, marker='')
+        plt.plot('t', 'y', data=df, marker='')
+        plt.plot('t', 'z', data=df, marker='')
+        plt.legend()
+        plt.show()
+
+        pickle.dump([time, pos_x, pos_y, pos_z, pos_R], open( "motion.p", "wb" ))
+
+    drone_vel = 0 # FIXME
 
     # Generate a depth map
     disparity = calc_disparity_map(intr, frame_prev, frame_cur)
@@ -105,9 +153,20 @@ def main():
     # FIXME ?
     # DJI Phantom-4 standard intrinsic matrix, per
     # https://stackoverflow.com/questions/47896876/camera-intrinsic-matrix-for-dji-phantom-4
-    intr = np.array([[1865.0, 0.0   , 2000.0],
-                     [0.0   , 1865.0, 1500.0],
-                     [0.0   , 0.0   , 1.0   ]])
+    # intr = np.array([[1865.0, 0.0   , 2000.0],
+    #                  [0.0   , 1865.0, 1500.0],
+    #                  [0.0   , 0.0   , 1.0   ]])
+    # Video input is (2160, 3840, 3). This looks like a phantom 4 (regular)
+    # running in 4K mode, per https://www.dji.com/phantom-4/info#specs  
+    intr = np.array([[2246.742, 0.0     , 1920.0],
+                     [0.0     , 2246.742, 1080.0],
+                     [0.0     , 0.0     , 1.0   ]])
+
+    # # https://gist.github.com/dbaldwin/21d55b134d71fd66e06dce8b91651a03
+    # intr = np.array([[2365.85284, 0         , 1970.24146],
+    #           [0         , 2364.17864, 1497.37745],
+    #           [0         , 0         , 1]])
+    frame_rate = 29 # either  24 / 25 / 30p
 
 
     log.info('Processing video file: {}, annotations file: {}'.format(args.video, args.annotations))
@@ -141,7 +200,7 @@ def main():
         if not annotations is None:
             bounding_boxes = annotations[annotations[:, 5] == frame_num]
 
-        process_frame(intr, 1.0 / constants.FRAME_RATE, frame_cur, frame_prev, bounding_boxes)
+        process_frame(intr, 1.0 / frame_rate, frame_cur, frame_prev, bounding_boxes)
 
     log.info('Processing video complete, {} frames processed'.format(frame_num))
 
