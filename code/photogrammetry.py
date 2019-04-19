60dbf79acbacb1dc595046b83e3844ec5813cc31
Nick Smith <nicholas_smith1@brown.edu>
2019-04-19 16:41:18 -0400

    WIP

--- code/photogrammetry.py
+++ code/photogrammetry.py
@@ -3,14 +3,23 @@ import numpy as np
 import matplotlib.pyplot as plt
 from helpers import (rotationMatrixToEulerAngles)
 
-def calc_drone_vel(intr, frame_prev, frame_cur, bounding_boxes):
+def calc_drone_pos_delta(intr, frame_prev, frame_cur, bounding_boxes):
+    """
+    @return (R, t)      the translation and rotation matrices describing
+                        the change in position from frame_prev to frame_cur.
+    """
     # Get key features
     #sift = cv2.xfeatures2d.SIFT_create()
     #surf = cv2.xfeatures2d.SURF_create()
     orb = cv2.ORB_create(nfeatures=1500)
 
+    # print('frame dim={}'.format(frame_cur.shape))
+
     kp_cur, desc_cur = orb.detectAndCompute(frame_cur, None)
     kp_prev, desc_prev = orb.detectAndCompute(frame_prev, None)
+
+    # TODO - remove any key features in bounding boxes with
+    # people in them
 
     # create BFMatcher object
     bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
@@ -18,15 +27,32 @@ def calc_drone_vel(intr, frame_prev, frame_cur, bounding_boxes):
     # Match descriptors. Hopefully improve RANSAC performance
     matches = bf.match(desc_prev,desc_cur)
 
-    # # Sort them in the order of their distance.
-    # matches = sorted(matches, key = lambda x:x.distance)
+    # # FLANN parameters
+    # FLANN_INDEX_KDTREE = 0
+    # index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+    # search_params = dict(checks=50)   # or pass empty dictionary
 
-    # Debug
-    img = cv2.drawMatches(frame_prev, kp_prev, frame_cur, kp_cur, matches, None)
-    plt.imshow(img),plt.show()
+    # flann = cv2.FlannBasedMatcher(index_params,search_params)
+    # matches = flann.knnMatch(desc_prev,desc_cur,k=2)
+
+    # # Sort them in the order of their distance.
+    matches = sorted(matches, key = lambda x:x.distance)
 
     kpc = np.empty((len(matches), 2))
     kpp = np.empty((len(matches), 2))
+
+    # Debug
+    if False:
+        # print(matches[-1:])
+        # kpc[0, 0] = kp_cur[matches[-1:][0].trainIdx].pt[0]
+        # kpc[0, 1] = kp_cur[matches[-1:][0].trainIdx].pt[1]
+        # kpp[0, 0] = kp_prev[matches[-1:][0].queryIdx].pt[0]
+        # kpp[0, 1] = kp_prev[matches[-1:][0].queryIdx].pt[1]
+
+        # print('{} {}'.format(kpc[0], kpp[0]))
+        img = cv2.drawMatches(frame_prev, kp_prev, frame_cur, kp_cur, matches, None)
+        plt.imshow(img),plt.show()
+
     for i in range(len(matches)):
         # print('-----')
         # print(matches[i].imgIdx)
@@ -34,27 +60,43 @@ def calc_drone_vel(intr, frame_prev, frame_cur, bounding_boxes):
         # print(matches[i].trainIdx)
 
         # FIXME - backwards?
-        kpc[i, 0] = kp_cur[matches[i].queryIdx].pt[0]
-        kpc[i, 1] = kp_cur[matches[i].queryIdx].pt[1]
-        kpp[i, 0] = kp_prev[matches[i].trainIdx].pt[0]
-        kpp[i, 1] = kp_prev[matches[i].trainIdx].pt[1]
+        # print('{} {} {}'.format(matches[i].queryIdx, len(kp_cur), len(kp_prev)))
+        # print(kp_cur[matches[i].queryIdx])
+        # print(kp_cur[matches[i].queryIdx].pt)
+        # kpc[i, 0] = kp_cur[matches[i].queryIdx].pt[0] # this was backwards
+        # kpc[i, 1] = kp_cur[matches[i].queryIdx].pt[1]
+        # kpp[i, 0] = kp_prev[matches[i].trainIdx].pt[0]
+        # kpp[i, 1] = kp_prev[matches[i].trainIdx].pt[1]
+        kpc[i, 0] = kp_cur[matches[i].trainIdx].pt[0]
+        kpc[i, 1] = kp_cur[matches[i].trainIdx].pt[1]
+        kpp[i, 0] = kp_prev[matches[i].queryIdx].pt[0]
+        kpp[i, 1] = kp_prev[matches[i].queryIdx].pt[1]
 
-    # Calculate the essential matrix
+    
+
+    # Calculate the essential matrix. Pose *should* be p2 to p1, in
+    # p2 coordinates
     focal = (intr[0, 0] + intr[1, 1]) / 2.0
     oc = (intr[0, 2], intr[1, 2])
+    # oc = (intr[1, 2], intr[0, 2]) # (row, col), rather than (x, y)? seems to minimize rotations
     E, mask = cv2.findEssentialMat(kpc, kpp, focal=focal, pp=oc, method=cv2.RANSAC, prob=0.999, threshold=1.0)
     
     retval, R, t, mask = cv2.recoverPose(E, kpc, kpp, intr, mask);
 
-    # TODO - convert to velocities using frame rate
-    # TODO - validate output
-
+    print('valid: {} / {}'.format(retval, len(kpc)))
     # print(R)
     # print(rotationMatrixToEulerAngles(R))
     # print(t)
 
+    # # debug
+    # if (retval / len(kpc)) < 0.5:
+    #     # when this happens, the matches are noticably bad. Should consider rebuilding
+    #     # with SIFT support?
+    #     img = cv2.drawMatches(frame_prev, kp_prev, frame_cur, kp_cur, matches, None)
+    #     plt.imshow(img),plt.show()
 
-    pass
+
+    return (retval / len(kpc)), R, t
 
 
 def calc_disparity_map(intr, frame_prev, frame_cur):
