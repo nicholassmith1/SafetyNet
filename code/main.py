ab07ec4f43f7d7eb5d299b4038d4fcfa9c44d20e
Nick Smith <nicholas_smith1@brown.edu>
2019-05-03 19:11:37 -0400

    Producing valid (but sparse) position data

--- code/main.py
+++ code/main.py
@@ -1,5 +1,3 @@
-
-
 import numpy as np
 import argparse
 import constants
@@ -8,143 +6,104 @@ import signal
 import sys
 import cv2
 import matplotlib.pyplot as plt
-import pickle
 from helpers import (rotationMatrixToEulerAngles)
+import os
+import tempfile
+import json
 
-from photogrammetry import (calc_drone_pos_delta,
-        calc_disparity_map,
-        calc_pedestrian_position,
-        calc_pedestrian_velocity,
-        calc_abs_vel)
-from visualization import visualize
 from segmentation import find_pedestrians
-from helpers import (collate_pedestrians,
-        classify_pedestrian,
-        estimate_epicenter)
+# from helpers import (get_points_from_ply,
+
+#         collate_pedestrians,
+#         classify_pedestrian,
+#         estimate_epicenter)
+import helpers
+from openMVG_pipeline import (openMVG_pipe, openMVS_pipe)
+from photogrammetry import (bounding_box_to_world_coord)
  
-log = logging.getLogger(__name__)
-
-# debug stuff
-time = [0.0]
-confidences = [0.0]
-pos_x = [0.0]
-pos_y = [0.0]
-pos_z = [0.0]
-# pos_R = [[[0.0, 0.0, 0.0], [0.0, 0.0, 0.0], [0.0, 0.0, 0.0]]]
-rot_x = [0.0]
-rot_y = [0.0]
-rot_z = [0.0]
-
-# last = 0 # debugging
-# t = 0
-
-def moving_average(data, N):
-    return np.convolve(data, np.ones((N,))/N, mode='same')
-
-def process_frame(intr, dt, frame_cur, frame_prev, bounding_boxes=None, out=None):
-    pedestrians_prev = None  # FIXME
-    epicenter_prev = None
-
-    # global t
-    # global last
-
-    # t = t + dt
-
-    # DEBUG skip ahead
-    # if time[len(time) - 1] < 30:
-    #     return
-
-
-
-    # Get an array of pedestrian bounding boxes.
-    if bounding_boxes is None:
-        bounding_boxes = find_pedestrians(frame_cur)
-
-    # Draw the bounding boxes
-    if out is not None:
-        frame = frame_cur
-        for bb in bounding_boxes:
-            frame = cv2.rectangle(frame,(bb[0],bb[1]), (bb[0] + bb[2], bb[1] + bb[3]), (0,255,0), 3)
-            # print((bb[0],bb[1]), (bb[0] + bb[2], bb[1] + bb[3]))
-        out.write(frame)
-
-    # Calculate the drone instanenous velocity, drone FOR
-    confidence, R, t = calc_drone_pos_delta(intr, frame_prev, frame_cur, bounding_boxes)
-    # debug stuff
-    time.append(time[len(time) - 1] + dt)
-    confidences.append(confidence)
-    pos_x.append(t[0])
-    pos_y.append(t[1])
-    pos_z.append(t[2])
-    rx, ry, rz = rotationMatrixToEulerAngles(R)
-    rot_x.append(rx)
-    rot_y.append(ry)
-    rot_z.append(rz)
-    # pos_R.append(R)
-    # if (confidence > 0.5):
-    #     pos_x.append(t[0])
-    #     pos_y.append(t[1])
-    #     pos_z.append(t[2])
-    #     pos_R.append(R)
-    # else:
-    #     pos_x.append(pos_x[len(pos_x) - 1])
-    #     pos_y.append(pos_y[len(pos_y) - 1])
-    #     pos_z.append(pos_z[len(pos_z) - 1])
-    #     pos_R.append(pos_R[len(pos_R) - 1])
-
-    drone_vel = 0 # FIXME
-
-    # Generate a depth map
-    disparity = calc_disparity_map(intr, frame_prev, frame_cur)
-
-    # Calculate the location of the ground, relative to the drone FOR,
-    #ground_norm = estimate_ground(disparity)
-
-    # Label bounding boxes as pedestrians. We must know both the current
-    # and the previous position of the pedestrian to estimate an instantenous
-    # velocity. 
-    pedestrians = collate_pedestrians(pedestrians_prev, bounding_boxes)
-
-    # FIXME
-    # Calculate the position and velocity of each pedestrian in the drone FOR
-    pedestrian_pos = calc_pedestrian_position(intr, pedestrians, disparity)
-    pedestrian_vel = calc_pedestrian_velocity(intr, pedestrians, disparity)
-
-    # Translate pedestrian velocities into real-world velocities
-    pedestrian_vel_abs = calc_abs_vel(intr, pedestrian_pos, pedestrian_vel, drone_vel)
-
-    # Classify each pedestrian as walking or jogging/running
-    pedestrian_class = classify_pedestrian(pedestrian_vel_abs)
-
-    # Predict an epicenter based on positions and velocities of pedestrians
-    epicenter = estimate_epicenter(epicenter_prev, pedestrian_class, pedestrian_pos, pedestrian_vel_abs)
-
-    visualize(intr, frame_cur, pedestrian_pos, pedestrian_vel, pedestrian_class, drone_vel, epicenter)
-
-def parse_annotations(annotation_file):
-    """
-    Interperts the specified annotation_file as SingleActionTrackingLabel
-    as specified by http://okutama-action.org/
-
-    @returns    numpy Nx9 array of integers, per the specification
-    """
-    # np.loadtxt might work, but there's mixed int and strings...
-
-    rtn = np.empty((0, 9), dtype=int)
-    with open(annotation_file) as f:
-        lines = f.readlines()
-        rtn = np.empty((len(lines), 9), dtype=int)
-        for i in range(len(lines)):
-            s = lines[i].split(' ')
-            rtn[i] = np.array([int(x) for x in s[:9]], dtype=int)
-    return rtn
+log = logging.getLogger('SafetyNet')
 
 def signal_handler(sig, frame):
         log.info('Exitting')
         sys.exit(0)
 
-def main():
 
+def process_video_frames(vidcap, out_dir, basename, frame_skip=60):
+    """
+    @return     total frames processed
+    """
+    rame_prev = None
+    frame_cur = None
+    
+    success, frame_prev = vidcap.read()
+    frame_num = 0
+
+    while success:
+        # Grab frame
+        for i in range(frame_skip):
+            frame_num = frame_num + 1
+            success,frame_cur = vidcap.read()
+        if not success or frame_cur is None:
+            break
+
+        log.debug('FRAME: {}'.format(frame_num, os.path.join(out_dir)))
+
+        frame_prev = frame_cur
+        cv2.imwrite(os.path.join(out_dir, '{}_{:05d}.png'.format(basename, frame_num)), frame_cur)
+
+    return frame_num
+
+
+def unravel_video(video_filename, out_dir, frame_skip=60, skip=False):
+    basename, ext = os.path.splitext(os.path.basename(video_filename))
+    vidcap = cv2.VideoCapture(video_filename)
+
+    img_dir = os.path.join(out_dir, 'images')
+    if not os.path.exists(img_dir):
+        os.makedirs(img_dir, exist_ok=True)
+
+    if skip:
+        frame_num = int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT))
+    else:
+        frame_num = process_video_frames(vidcap, img_dir, basename, frame_skip=frame_skip)
+
+    vidcap.release()
+
+    return frame_num, img_dir
+
+
+def pose_estimation(img_dir, out_dir, skip=False):
+    # generate workspace
+    pose_dir = os.path.join(out_dir, 'pose')
+    if not os.path.exists(pose_dir):
+        os.makedirs(pose_dir, exist_ok=True)
+
+    # Establish output
+    sfm_data_bin = os.path.join(pose_dir, "sfm_data.bin")
+    sfm_data_json = os.path.join(pose_dir, "sfm_data.json")
+
+    if not skip:
+        sfm_data = openMVG_pipe(img_dir, pose_dir, sfm_data)
+
+    return sfm_data_bin, sfm_data_json
+
+
+def depth_estimation(sfm_data_filename, out_dir, skip=False):
+    # Generate workspace
+    depth_dir = os.path.join(out_dir, 'depth')
+    if not os.path.exists(depth_dir):
+        os.makedirs(depth_dir, exist_ok=True)
+
+    # Establish output
+    pc_filename = os.path.join(depth_dir, "scene_dense.ply")
+    if not skip:
+        # Run the openMVS pipeline
+        pc_filename = openMVS_pipe(depth_dir, sfm_data_filename, pc_filename)
+
+    return pc_filename
+
+
+def main():
     # Argument parsing
     parser = argparse.ArgumentParser(description='SafetyNet')
     parser.add_argument('video', metavar='VIDEOFILE', type=str,
@@ -152,6 +111,12 @@ def main():
     parser.add_argument('-a', dest='annotations', type=str, default=None,
                     required=False, help='Overrides the neural network calculation of pedestrian \
                     bounding boxes and uses annotations for bounding-box generation')
+    parser.add_argument('--depth_pose_downsample', type=int, default=60,
+            help='Only use 1 in N views for density estimation, which tends to be computationally intense')
+    parser.add_argument('--skip_unravel', action='store_true', help='Skip the generation of frames from a video')
+    parser.add_argument('--skip_pose', action='store_true', help='Skip the openMVG pipeline')
+    parser.add_argument('--skip_depth', action='store_true', help='Skip the openMVS pipeline')
+
     args = parser.parse_args()
 
     # Install signal handlers for early termination
@@ -163,22 +128,15 @@ def main():
 
     logging.basicConfig(level=logging.DEBUG)
 
-    # FIXME ?
-    # DJI Phantom-4 standard intrinsic matrix, per
-    # https://stackoverflow.com/questions/47896876/camera-intrinsic-matrix-for-dji-phantom-4
-    # intr = np.array([[1865.0, 0.0   , 2000.0],
-    #                  [0.0   , 1865.0, 1500.0],
-    #                  [0.0   , 0.0   , 1.0   ]])
-    # Video input is (2160, 3840, 3). This looks like a phantom 4 (regular)
-    # running in 4K mode, per https://www.dji.com/phantom-4/info#specs  
-    intr = np.array([[2246.742, 0.0     , 1920.0],
-                     [0.0     , 2246.742, 1080.0],
-                     [0.0     , 0.0     , 1.0   ]])
-
+    # # DJI Phantom-4 standard intrinsic matrix, per
+    # # https://stackoverflow.com/questions/47896876/camera-intrinsic-matrix-for-dji-phantom-4
+    # #
+    # # Video input is (2160, 3840, 3). This looks like a phantom 4 (regular)
+    # # running in 4K mode, per https://www.dji.com/phantom-4/info#specs  
     # # https://gist.github.com/dbaldwin/21d55b134d71fd66e06dce8b91651a03
-    # intr = np.array([[2365.85284, 0         , 1970.24146],
-    #           [0         , 2364.17864, 1497.37745],
-    #           [0         , 0         , 1]])
+    # intr = np.array([[2246.742, 0.0     , 1920.0],
+    #                  [0.0     , 2246.742, 1080.0],
+    #                  [0.0     , 0.0     , 1.0   ]])
     frame_rate = 30 # either  24 / 25 / 30p
 
 
@@ -187,67 +145,149 @@ def main():
     # Load ground-truth meta-data, if it exists
     annotations = None
     if args.annotations != None:
-        annotations = parse_annotations(args.annotations)
+        annotations = helpers.parse_annotations(args.annotations)
+        # TODO get data either from annotation, or elsewhere
+        # pedestrians_2d = np.array((1, 6)) # [p_id, frame_id, xmin, ymin, xmax, ymax]
+        pedestrians_2d = np.zeros((len(annotations), 6))
+        pedestrians_2d[:, 0]  = annotations[:,0]
+        pedestrians_2d[:, 1]  = annotations[:,5]
+        pedestrians_2d[:, 2:6]  = annotations[:, 1:5]
+        # all_frame_ids = np.sort(np.unique(pedestrian_2d[:, 1]))
 
-    # Load the video stream and any ground-truth meta-data available.
-    # Separate input into individual frames and process one at a
-    # time
+    # Create a space to work in the temporary filesystem
+    basename, ext = os.path.splitext(os.path.basename(args.video))
+    out_dir = tempfile.gettempdir()
+    out_dir = os.path.join(out_dir, 'SafetyNet', basename)
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir, exist_ok=True)
 
-    frame_prev = None
-    frame_cur = None
+    # Turn the video into a sequence of images
+    frame_num, img_dir = unravel_video(args.video, out_dir, skip=args.skip_unravel)
 
-    vidcap = cv2.VideoCapture(args.video)
-    success, frame_prev = vidcap.read()
-    frame_num = 0
-    skip = 60
+    # Run the openMVG pipeline.
+    sfm_bin_filename, sfm_data_filename = pose_estimation(img_dir, out_dir, skip=args.skip_pose)
 
-    # Create output video for debugging
-    # out = cv2.VideoWriter('{}_out.avi'.format(args.video),
-    #         cv2.VideoWriter_fourcc('M','J','P','G'), 10,
-    #         (frame_prev.shape[0],frame_prev.shape[1]))
-    fourcc = cv2.VideoWriter_fourcc(*'XVID')
-    out = cv2.VideoWriter('output.avi',fourcc, frame_rate / skip, (frame_prev.shape[1],frame_prev.shape[0]))
+    # Run the openMVS pipeline
+    pc_filename = depth_estimation(sfm_bin_filename, out_dir, skip=args.skip_depth)
 
-    while success:
-        # Grab frame
-        for i in range(skip):
-            frame_num = frame_num + 1
-            success,frame_cur = vidcap.read()
-        if not success or frame_cur is None:
-            break
+    # Get the point cloud
+    point_cloud = helpers.get_points_from_ply(pc_filename)
 
-        log.debug('FRAME: {}'.format(frame_num))
+    # bb = helpers.get_bounding_boxes_for_frame('../data/1.1.5.txt', 1080)
 
-        # Grab all annotations for this frame number (column #5)
-        bounding_boxes = None
-        if not annotations is None:
-            bounding_boxes = annotations[annotations[:, 5] == frame_num]
+    sfm_json = None
+    with open(sfm_data_filename, 'r') as file:
+        sfm_json = json.load(file) 
 
-        process_frame(intr, 1.0 * skip / frame_rate, frame_cur, frame_prev, bounding_boxes, out)
-        frame_prev = frame_cur
+    # Get calculated intrinsics
+    intrinsics = helpers.get_camera_intrinsic_from_sfm_data(sfm_json)
+    K = np.zeros((3,3))
+    K[0, 0] = intrinsics['focal_length']
+    K[1, 1] = intrinsics['focal_length']
+    K[0, 2] = intrinsics['principal_point'][0]
+    K[1, 2] = intrinsics['principal_point'][1]
+    K[2, 2] = 1
 
-        # success = frame_num < 30
-    vidcap.release()
-    out.release()
+    # drone_data_avail, drone_pose, and drone_rot are all indexed by frame number
+    drone_data_avail = np.zeros(frame_num)
+    drone_pose = np.zeros((frame_num, 3))
+    drone_rot = np.zeros((frame_num, 3, 3))
 
-    # DEBUG
-    import matplotlib.pyplot as plt
-    import pandas as pd
+    fid_to_pid_map = helpers.get_frame_id_to_pose_id_map(sfm_json)
+    for frame_id, pose_id in fid_to_pid_map:
+        drone_data_avail[frame_id] = 1
+        drone_pose[frame_id], drone_rot[frame_id] = helpers.get_camera_pose_from_sfm_data(sfm_json, pose_id)
 
-    ma_pos_x = moving_average(np.array(pos_x), 15)
-    ma_pos_y = moving_average(np.array(pos_y), 15)
-    ma_pos_z = moving_average(np.array(pos_z), 15)
+    print('drone pose estimation complete')
 
-    df=pd.DataFrame({'t': time, 'x': ma_pos_x, 'y': ma_pos_y, 'z': ma_pos_z })
-    plt.plot('t', 'x', data=df, marker='')
-    plt.plot('t', 'y', data=df, marker='')
-    plt.plot('t', 'z', data=df, marker='')
-    plt.legend()
-    plt.show()
+    # Sparsely populate pedestrian poses
+    pedestrians_pose = np.zeros((len(pedestrians_2d), 5)) # [frame_id, p_id, x, y, z]
+    for i, p2d in enumerate(pedestrians_2d):
+        frame_id = int(p2d[1])
+        pedestrians_pose[i, 0] = p2d[1]  # copy frame id
+        pedestrians_pose[i, 1] = p2d[0]  # copy pedestrian id
 
-    pickle.dump([time, confidences, pos_x, pos_y, pos_z, rot_x, rot_y, rot_z], open( "motion.p", "wb" ))
+        # C, R = helpers.get_camera_pose_from_sfm_data('test_sfm_data.json', 1)
+        # print(frame_id)
+        if not drone_data_avail[frame_id]:
+            continue
+        
+        C = drone_pose[frame_id]
+        R = drone_rot[frame_id]
+        try:
+            pedestrians_pose[i, 2:] = bounding_box_to_world_coord(K, R, C, p2d[2:6], point_cloud)
+            # print(pedestrians_pose[i, 2:])
+        except:
+            print('failure')
+            print(R)
+            print(C)
+            print(p2d[2:6])
 
-    log.info('Processing video complete, {} frames processed'.format(frame_num))
+    print('pedestrian pose estimation complete')
+
+
+    # TODO - calculate velocity
+    pedestrians_vel = np.zeros((len(pedestrians_2d), 4)) # [frame_id], p_id, vx, vy, vz]
+    # for ...
+
+    helpers.serial_safetynet_out(out_dir, frame_num,
+            drone_pose, drone_rot,
+            pedestrians_pose, pedestrians_vel,
+            K, frame_rate)
+    # frames = []
+    # for frame_id in range(frame_num):
+    #     drone = {
+    #         "pose" : drone_pose[frame_id],
+    #         "R" : drone_rot[frame_id],
+    #         "vel" : np.zeros(3) # TODO - placeholder
+    #     }
+    #     # Get pedestrian data
+    #     pedestrians = []
+    #     for pdata in pedestrians_pose[pedestrians_pose[:, 0] == frame_id]:
+    #         p = {
+    #             "id" : pdata[1],
+    #             "pose" : pdata[2:5],
+    #             "vel" : pdata[5:8]
+    #         }
+    #         pedestrians.append(p)
+    #     # Assemble frame
+    #     frame = {
+    #         "frame_id" : frame_id,
+    #         "drone" : drone,
+    #         "epicenter" : np.zeros(3),
+    #         "pedestrians": pedestrians
+    #     }
+
+    # data = {}
+    # data['scale'] = 1.0 # placeholder
+    # data['timedelta'] = 1 / frame_rate
+    # data['K'] = K
+    # data['frames'] = frames
+    # data_dump = json.dumps(data, cls=helpers.NumpyEncoder)
+    # with open(os.path.join(out_dir, 'safetynet.json'), 'w') as f:
+    #     json.dump(data_dump, f)
+
+    
+
+    # Generate a 3D position for each bounding box
+    # TODO - may want this more flexible so you can run the
+    # the Densify operation on different deltas
+    # pts = np.empty() # all dense points in scene
+    # pedestrians_for_frame = np.empty((,5))  # id, xmin, ymin, xmax, ymax
+
+    # helpers.get_points_from_ply('scene_dense.ply')
+    # P, R = helpers.get_camera_pose_from_sfm_data('test_sfm_data.json', 1)
+    # intrinsics = helpers.get_camera_intrinsic_from_sfm_data('test_sfm_data.json')
+    # bb = helpers.get_bounding_boxes_for_frame('../data/1.1.5.txt', 1080)
+
+    # bounding_box = bb[6][1:5]
+    # # why do the focal_length get divided by 2?
+    # frustrum_norms = helpers.camera_pose_to_frustrum_norms(R, intrinsics['focal_length'] / 2, intrinsics['principal_point'], bounding_box)
+    # pts = helpers.get_points_from_ply('scene_dense.ply')
+
+    # in_frustrum = helpers.get_points_inside_frustrum(pts - P, frustrum_norms)
+    # #np.where(in_pts == True)
+    # p2 = pts[in_frustrum]
 
 
 
--- code/photogrammetry.py
+++ code/photogrammetry.py
@@ -1,124 +1,92 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-from helpers import (rotationMatrixToEulerAngles)
+import helpers
 
-
-def get_matching_points(frame_prev, frame_cur):
-#sift = cv2.xfeatures2d.SIFT_create()
-    #surf = cv2.xfeatures2d.SURF_create()
-    orb = cv2.ORB_create(nfeatures=1500)
-
-    # print('frame dim={}'.format(frame_cur.shape))
-
-    kp_cur, desc_cur = orb.detectAndCompute(frame_cur, None)
-    kp_prev, desc_prev = orb.detectAndCompute(frame_prev, None)
-
-    # TODO - remove any key features in bounding boxes with
-    # people in them
-
-    # create BFMatcher object
-    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
-
-    # Match descriptors. Hopefully improve RANSAC performance
-    matches = bf.match(desc_prev, desc_cur)
-
-    # # FLANN parameters
-    # FLANN_INDEX_KDTREE = 0
-    # index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
-    # search_params = dict(checks=50)   # or pass empty dictionary
-
-    # flann = cv2.FlannBasedMatcher(index_params,search_params)
-    # matches = flann.knnMatch(desc_prev,desc_cur,k=2)
-
-    # # Sort them in the order of their distance.
-    matches = sorted(matches, key = lambda x:x.distance)
-
-    kpc = np.empty((len(matches), 2))
-    kpp = np.empty((len(matches), 2))
-
-    matches = matches[:10]
-
-    for i in range(len(matches)):
-        # print('-----')
-        # print(matches[i].imgIdx)
-        # print(matches[i].queryIdx)
-        # print(matches[i].trainIdx)
-
-        # FIXME - backwards?
-        # print('{} {} {}'.format(matches[i].queryIdx, len(kp_cur), len(kp_prev)))
-        # print(kp_cur[matches[i].queryIdx])
-        # print(kp_cur[matches[i].queryIdx].pt)
-        # kpc[i, 0] = kp_cur[matches[i].queryIdx].pt[0] # this was backwards
-        # kpc[i, 1] = kp_cur[matches[i].queryIdx].pt[1]
-        # kpp[i, 0] = kp_prev[matches[i].trainIdx].pt[0]
-        # kpp[i, 1] = kp_prev[matches[i].trainIdx].pt[1]
-        kpc[i, 0] = kp_cur[matches[i].trainIdx].pt[0]
-        kpc[i, 1] = kp_cur[matches[i].trainIdx].pt[1]
-        kpp[i, 0] = kp_prev[matches[i].queryIdx].pt[0]
-        kpp[i, 1] = kp_prev[matches[i].queryIdx].pt[1]
-
-    # Debug
-    if False:
-        # print(matches[-1:])
-        # kpc[0, 0] = kp_cur[matches[-1:][0].trainIdx].pt[0]
-        # kpc[0, 1] = kp_cur[matches[-1:][0].trainIdx].pt[1]
-        # kpp[0, 0] = kp_prev[matches[-1:][0].queryIdx].pt[0]
-        # kpp[0, 1] = kp_prev[matches[-1:][0].queryIdx].pt[1]
-        # frame_prev = cv2.resize(frame_prev, None, fx=0.5, fy=0.5)
-        # frame_cur = cv2.resize(frame_cur, None, fx=0.5, fy=0.5)
-
-        print(matches[0])
-        print('{} {}'.format(kpp[0], kpc[0]))
-        img = cv2.drawMatches(frame_prev, kp_prev, frame_cur, kp_cur, matches, None)
-        plt.imshow(img),plt.show()
-
-    return kpp, kpc
-
-def calc_drone_pos_delta(intr, frame_prev, frame_cur, bounding_boxes):
+def get_points_inside_frustrum(points, frustrum_norms):
     """
-    @return (R, t)      the translation and rotation matrices describing
-                        the change in position from frame_prev to frame_cur.
+    @param points       numpy array (Nx3) of points in the scene
+    @param frustrum_nroms   numpy array (4x3) representing the planar normal vectors
+                        of the frustrum
+    @return             numpy array (Nx1) of bool, True if the point is within the
+                        frustrum, False otherwise
     """
+    # Dot each point onto each frustrum norm to get it's projection.
+    # Results in an Nx4 matrix
+    dots = np.dot(points, frustrum_norms.T)
+    # For a point to be within the frustrum, the projection on each normal
+    # vector must be positive
+    return (dots >= 0).all(axis=1)
 
-    # Get key features
-    kpp, kpc = get_matching_points(frame_prev, frame_cur)
+def camera_pose_to_frustrum_norms(K, R, C, bounding_box):
+    """
+    @param R            numpy array (3x3) representing the camera optical center rotation
+    @param focal        focal length in pixels
+    @param cc           principle point (Cx, Cy) in pixels
+    @param bounding_box bounding boxes, in pixel, in the form (x0, y0, x1, y1)
+    @return             numpy array (4x3) of vectors representing the normal vectors
+                        of the planes that make up the frustrum of the specified bounding box
+                        applied at the specified camera pose.
+    """
+    v0 = helpers.pixel_to_real_world(K, R, C, np.asarray((bounding_box[0], bounding_box[1])))
+    v1 = helpers.pixel_to_real_world(K, R, C, np.asarray((bounding_box[2], bounding_box[1])))
+    v2 = helpers.pixel_to_real_world(K, R, C, np.asarray((bounding_box[2], bounding_box[3])))
+    v3 = helpers.pixel_to_real_world(K, R, C, np.asarray((bounding_box[0], bounding_box[3])))
+    # v0 = np.asarray((bounding_box[0] - cc[0], bounding_box[1] - cc[1], focal))
+    # v1 = np.asarray((bounding_box[2] - cc[0], bounding_box[1] - cc[1], focal))
+    # v2 = np.asarray((bounding_box[2] - cc[0], bounding_box[3] - cc[1], focal))
+    # v3 = np.asarray((bounding_box[0] - cc[0], bounding_box[3] - cc[1], focal))
 
+    # TODO - account for radial distortion?
+
+    # explanation of coordinates https://github.com/openMVG/openMVG/issues/788
+    # apply rotations
+    # v0 = np.matmul(R, v0)
+    # v1 = np.matmul(R, v1)
+    # v2 = np.matmul(R, v2)
+    # v3 = np.matmul(R, v3)
+    # v0 = np.dot(R, v0)
+    # v1 = np.dot(R, v1)
+    # v2 = np.dot(R, v2)
+    # v3 = np.dot(R, v3)
+    # # apply translations
+    # v0 = v0 + P
+    # v1 = v1 + P
+    # v2 = v2 + P
+    # v3 = v3 + P
+
+    # print(v0 / v0[2] * 0.68)
+    # print(v1 / v1[2] * 0.68)
+    # print(v2 / v2[2] * 0.68)
+    # print(v3 / v3[2] * 0.68)
+    # print(v0 / v0[2] * 0.668)
+    # print(v1 / v1[2] * 0.668)
+    # print(v2 / v2[2] * 0.668)
+    # print(v3 / v3[2] * 0.668)
+
+    # why would i need to invert the rotation of the pose?
+    # Ri = np.linalg.inv(R)
+    # v0 = np.matmul(Ri, v0)
+    # v1 = np.matmul(Ri, v1)
+    # v2 = np.matmul(Ri, v2)
+    # v3 = np.matmul(Ri, v3)
+
+    # watch the winding consistency (normal vectors should point inwards)
+    n = np.empty((4, 3))
+    n[0,:] = np.cross(v0, v1)
+    n[1,:] = np.cross(v1, v2)
+    n[2,:] = np.cross(v2, v3)
+    n[3,:] = np.cross(v3, v0)
+
+    return n
+    # return (n, np.asarray([v0, v1, v2, v3]))
+
+def bounding_box_to_world_coord(K, R, C, bounding_box, point_cloud):
+    norms = camera_pose_to_frustrum_norms(K, R, C, bounding_box)
+    inner_pts = get_points_inside_frustrum(point_cloud, norms)
+    # FIXME? For the moment, just calculate based on average of
+    # surrounding groud
+
+
+    return np.mean(point_cloud[inner_pts], axis=0)
     
-
-    # Calculate the essential matrix. Pose *should* be p2 to p1, in
-    # p2 coordinates
-    focal = (intr[0, 0] + intr[1, 1]) / 2.0
-    oc = (intr[0, 2], intr[1, 2])
-    # oc = (intr[1, 2], intr[0, 2]) # (row, col), rather than (x, y)? seems to minimize rotations
-    E, mask = cv2.findEssentialMat(kpc, kpp, focal=focal, pp=oc, method=cv2.RANSAC, prob=0.999, threshold=1.0)
-    
-    retval, R, t, mask = cv2.recoverPose(E, kpc, kpp, intr, mask);
-
-    print('valid: {} / {}'.format(retval, len(kpc)))
-    # print(R)
-    print(rotationMatrixToEulerAngles(R))
-    print(t)
-
-    # # debug
-    # if (retval / len(kpc)) < 0.5:
-    #     # when this happens, the matches are noticably bad. Should consider rebuilding
-    #     # with SIFT support?
-    #     img = cv2.drawMatches(frame_prev, kp_prev, frame_cur, kp_cur, matches, None)
-    #     plt.imshow(img),plt.show()
-
-
-    return (retval / len(kpc)), R, t
-
-
-def calc_disparity_map(intr, frame_prev, frame_cur):
-    pass
-
-def calc_pedestrian_position(intr, pedestrians, disparity):
-    pass
-
-def calc_pedestrian_velocity(intr, pedestrians, disparity):
-    pass
-
-def calc_abs_vel(intr, pedestrian_pos, pedestrian_vel, drone_vel):
-    pass
